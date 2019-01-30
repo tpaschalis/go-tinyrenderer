99fac9a27b523ba13cd4dc44ec336faa4827a697
Paschalis T <paschalist0@gmail.com>
2019-01-30 16:32:01 +0200

    Lesson 1: Bresenham’s Line Drawing Algorithm - First Attempt

--- main.go
+++ main.go
@@ -7,6 +7,26 @@ import "image"
 import "image/color"
 import "image/png"
 
+func line(x0, y0, x1, y1 int, canvas *image.RGBA, c color.RGBA) {
+	a := 0.01 // run it for a = 0.1 as well, to see the line thinning out
+	for t := 0.0; t < 1.0; t += a {
+		x := float64(x0)*(1.0-t) + float64(x1)*t
+		y := float64(y0)*(1.0-t) + float64(y1)*t
+		canvas.Set(int(x), int(y), c)
+	}
+}
+
+func flipVertically(canvas *image.RGBA) *image.RGBA {
+	bounds := canvas.Bounds()
+	flipped := image.NewRGBA(image.Rect(0, 0, bounds.Max.X, bounds.Max.Y))
+	for i := 0; i <= bounds.Max.X; i++ {
+		for j := 0; j <= bounds.Max.Y; j++ {
+			flipped.Set(i, bounds.Max.Y-j-1, canvas.At(i, j))
+		}
+	}
+	return flipped
+}
+
 func main() {
 	w, h := 100, 100
 
@@ -21,8 +41,15 @@ func main() {
 	}
 	defer f.Close()
 
+	for i := 0; i < w; i++ {
+		for j := 0; j < h; j++ {
+			img.Set(i, j, color.RGBA{0, 0, 0, 255})
+		}
+	}
+
 	img.Set(52, 41, red)
-	_ = white
+	line(13, 20, 80, 40, img, white)
+	img = flipVertically(img)
 
 	png.Encode(f, img)
 
